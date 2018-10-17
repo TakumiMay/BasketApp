7303b9a4faa5ad9bb3108141285af95002c885be
Dan3s <daniel.guzman1@correo.icesi.edu.co>
2018-10-16 23:47:56 -0500

    Red Black Tree added

--- Basketball/src/structures/NodeRBT.java
+++ Basketball/src/structures/NodeRBT.java
@@ -1,5 +1,15 @@
 package structures;
 
+
+
 public class NodeRBT {
 
+	
+	int key = -1, color = RBT.BLACK;
+	NodeRBT left = RBT.nil, right = RBT.nil, parent = RBT.nil;
+
+    NodeRBT(int key) {
+        this.key = key;
+    }
+    
 }
