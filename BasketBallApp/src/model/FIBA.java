22d38fe7364b95c9303269b2ac4baba4c29770b1
Dan3s <daniel.guzman1@correo.icesi.edu.co>
2018-10-25 15:46:26 -0500

    Test cases design added

--- BasketBallApp/src/model/FIBA.java
+++ BasketBallApp/src/model/FIBA.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.util.Hashtable;
+
+import structures.AVL;
+
 public class FIBA {
+	 
+	
+	//AVL<Hashtable<Integer, String>> avl_points = new AVL<new Hashtanle>>;
+	
 
 }
